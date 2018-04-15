2fe32ebe20dba1c4693ee36fdaf92bbcfc71fbdc
SirMwazv <mwazvitam@gmail.com>
2018-04-15 15:10:48 +0200

    Skeleton of tests complete

--- Morabaraba9001.Test/Tests.cs
+++ Morabaraba9001.Test/Tests.cs
@@ -1,8 +1,88 @@
 ﻿using System;
+using NUnit.Framework;
+using System.Linq;
+
 
 namespace Morabaraba9001.Test
 {
+    [TestFixture]
     public class Tests
     {
+        #region Placing
+        [Test]
+        public void EmptyBoardOnGameStart()
+        {            
+        }
+        [Test]
+        public void PlayerWithDarkCowsPlaysFirst()
+        {
+        }
+        [Test]
+        public void CowsCanOnlyBePlacedOnEmptySpaces()
+        {
+        }
+        [Test]
+        public void MaximumOf12PlacementsPerPlayer()
+        {
+        }
+        [Test]
+        public void CowsCannotBeMovedDuringPlacement()
+        {
+        }
+        #endregion
+
+        #region Moving
+        [Test]
+        public void CowCanOnlyMoveToAdjacentSpace()
+        {
+        }
+        [Test]
+        public void CowCanOnlyMoveOnEmptySpace()
+        {
+        }
+        [Test]
+        public void MovingDoesNotIncreaseOrDecreaseNumberOfCowsOnTheBoard()
+        {
+        }
+        #endregion
+
+        #region Flying
+        [Test]
+        public void CowsCanOnlyMoveToEmptySpaceIfThreeOfThoseCowsRemain()
+        {
+        }
+        #endregion
+
+        #region In General
+        [Test]
+        public void MillFormedByThreeOfTheSameColourCowInALine()
+        {
+        }
+        [Test]
+        public void MillNotFormedByDifferentColouredCowsInALine()
+        {
+        }
+        [Test]
+        public void ConnectedSpacesWithCowsDoNotFormALine()
+        {
+        }
+        [Test]
+        public void CanOnlyShootOnTurnThatMillIsCompleted()
+        {
+        }
+        [Test]
+        public void CannotShootACowInAMillWhenNonMillCowsExist()
+        {
+        }
+        [Test]
+        public void CanShootACowInAMillWhenNonMillCowsDoNotExist()
+        {
+        }
+        [Test]
+        public void PlayerCannotShootOwnCows()
+        {
+        }
+        #endregion
+
     }
 }
